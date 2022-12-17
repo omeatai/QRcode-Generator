adb79f808ebc25001adf5b7e7c343ed7b63ef824
IFEANYI OMEATA <32337103+omeatai@users.noreply.github.com>
2022-12-17 23:22:42 +0100

    QRcode Generator

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import QRCode from "qrcode.react";
 
 function App() {
   const [url, setUrl] = useState("https://www.google.com");
+  const [fileType, setFiletype] = useState("png");
 
   const handleDownload = (e) => {
     const canvas = e.target;
-    const imageData = canvas.toDataURL("image/gif");
+    const imageData = canvas.toDataURL(`image/${fileType}`);
     const link = document.createElement("a");
-    link.download = "my-image.gif";
+    link.download = `my-image.${fileType}`;
     link.href = imageData;
     link.click();
   };
 
   const handleChange = (e) => {
     setUrl(e.target.value);
+  };
+
+  const handleChoice = (e) => {
+    setFiletype(e.target.value);
+    console.log(e.target.value);
   };
 
   return (
@@ -27,7 +33,7 @@ function App() {
           Enter QRcode Data
         </label>
         <input
-          className="mb-8 p-2 w-[80vw] min-[500px]:w-[60vw] min-w-full text-lg text-gray-500 outline-none border-4 rounded-lg text-center hover:border-gray-500"
+          className="mb-6 p-2 w-[80vw] min-[500px]:w-[60vw] min-w-full text-lg text-gray-500 outline-none border-4 rounded-lg text-center hover:border-gray-500"
           onChange={handleChange}
           type="text"
           id="qr"
@@ -43,9 +49,18 @@ function App() {
           bgColor="#FFFFFF"
         />
       </div>
-      <p className="text-2xl font-light text-white">
+      <p className="text-2xl font-light text-white mb-2">
         Click the QRcode to download it
       </p>
+      <select
+        onChange={handleChoice}
+        className="py-2 px-8 font-bold outline-none rounded-md"
+      >
+        <option value="png">PNG</option>
+        <option value="jpeg">JPEG</option>
+        <option value="jpg">JPG</option>
+        <option value="gif">GIF</option>
+      </select>
     </div>
   );
 }
